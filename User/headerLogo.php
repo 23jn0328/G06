00c8bd09897d63167fbdc8adb92842971ec2e99d
野口勇人 <23jn0328@jec.ac.jp>
2024-12-03 16:23:17 +0900

    絶対プルしろ

---
+++ User/headerLogo.php
@@ -0,0 +1,7 @@
+<header>
+        <div id="logo">
+            <a href="イベントの閲覧と選択.html">
+                <img src="Z:\xampp\htdocs\G06_source\G06-1\User\img\image.png" alt="">
+            </a>
+        </div>
+</header>
---
+++ User/イベントの閲覧と選択.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>イベントリスト</title>
+    <link rel="stylesheet" href="イベントの閲覧と選択.css">
+</head>
+<body>
+
+    <div class="container">
+    <header>
+        <div id="logo">
+            <a href="イベントの閲覧と選択.php">
+                <img src="img/image.png" alt="WARIPAYロゴ">
+            </a>
+        </div>
+    </header>
+        <!-- PHPコードでヘッダーロゴを挿入 -->
+        <?php include "headerLogo.php"; ?>
+        
+        <!-- イベント作成ボタン -->
+        <button class="gradient-btn" onclick="location.href='イベント作成.php'">イベントを作成</button>
+
+        <!-- イベントリスト -->
+        <div class="event-list">
+            <div class="event-item" onclick="location.href='出来事の閲覧と選択.php'">
+                <div class="event-name">沖縄旅行</div>
+                <div class="event-date">開始日時: 2024年1月1日</div>
+            </div>
+            <div class="event-item" onclick="location.href='出来事の閲覧と選択.php'">
+                <div class="event-name">合宿</div>
+                <div class="event-date">開始日時: 2024年2月1日</div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
