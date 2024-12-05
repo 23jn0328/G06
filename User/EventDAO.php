d79601c0f99bf8b19b5f968d4d9d375dd3587f5c
野口勇人 <23jn0328@jec.ac.jp>
2024-12-05 16:36:39 +0900

    Dao追加

---
+++ User/EventDAO.php
@@ -0,0 +1,17 @@
+<?php
+    require_once 'DAO.php';
+
+class Event
+    {
+        public int    $EID;    //会員ID
+        public int $ID;       //メールアドレス
+        public string $EventName;  //会員名
+        public string $EventDate;     //郵便番号
+        public string $EventStart;     //住所
+        
+    }
+
+    class EventDAO
+    {
+        
+    }
---
+++ User/EventMemberDAO.php
@@ -0,0 +1,16 @@
+
+<?php
+    require_once 'DAO.php';
+
+class Event
+    {
+        public int  $EMID;   
+        public int $EID;       
+        public string $EventMemberName;  
+        
+    }
+
+    class EventMemberDAO
+    {
+        
+    }
