fd82d9d04f8b510bedd2418ef004cf1766dd0ce8
鈴木輝 <23jn0328@jec.ac.jp>
2024-12-10 16:41:06 +0900

    イベントメンバー途中

--- User/EventMemberDAO.php
+++ User/EventMemberDAO.php
@@ -1,16 +1,30 @@
-
 <?php
     require_once 'DAO.php';
 
 class Event
     {
-        public int  $EMID;   
-        public int $EID;       
+        public string $EMID;   
+        public string $EID;       
         public string $EventMemberName;  
         
     }
 
     class EventMemberDAO
     {
-        
+        public function getEventMembers(string $EID)
+        {
+            $dbh=DAO::get_db_connect();
+
+            $sql="SELECT em.id AS EMID, 
+                em.event_id AS EID, 
+                m.name AS EventMemberName
+                FROM 
+                event_members em
+                INNER JOIN members m 
+                ON em.member_id = m.id
+                WHERE em.event_id = :eventId";
+
+            
+        }
     }
+
