c69d51d056da189d13dab3e35ec8dc4db9d311eb
川嶋秀斗 <23jn0328@jec.ac.jp>
2024-12-12 15:27:09 +0900

    ロゴcontainerの中に変更

--- User/パスワード再設定.php
+++ User/パスワード再設定.php
@@ -4,23 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="パスワード再設定.css">
-    <title>パスワードの再設定</title>
+    <title>パスワードの再設定 </title>
 </head>
 <body>
     <div id="main-container">
-    <div id="logo">
-            <a href="イベントの閲覧と選択.php">
-                <img src="img/image.png" alt="WARIPAYロゴ">
-            </a>
+    
+        <div class="container">
+        <div id="logo">
+                <a href="イベントの閲覧と選択.php">
+                    <img src="img/image.png" alt="WARIPAYロゴ">
+                </a>
         </div>
-    <div class="container">
-        <h1>パスワードの再設定</h1>
-        <form>
-            <input type="password" placeholder="新しいパスワードを入力" required>
-            <input type="password" placeholder="確認のため、もう一度パスワードを入力してください" required>
-            <button type="submit">パスワードの再設定</button>
-        </form>
-    </div>
+            <h1>パスワードの再設定</h1>
+            <form>
+                <input type="password" placeholder="新しいパスワードを入力" required>
+                <input type="password" placeholder="確認のため、もう一度パスワードを入力してください" required>
+                <button type="submit">パスワードの再設定</button>
+            </form>
+        </div>
     </div>
 </body>
 </html>
