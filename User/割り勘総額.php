73ab7e6e2c2249004a4fba4f6ee38b70af747749
高橋生己 <23jn0330@jec.ac.jp>
2025-01-28 10:58:58 +0900

    総額

--- User/割り勘総額.php
+++ User/割り勘総額.php
@@ -72,36 +72,36 @@
             <li class="member-item">
                 <a>ひかる</a>
                 <div>
-                <a href="割り勘明細受け取りhikaru.php"><span class="payment-amount">¥37,000</span></a>
-                <a href="割り勘明細hikaru.php"><span class="payment-amount2">¥2500</span></a>
+                <a href="割り勘明細受け取りhikaru.php"><span class="payment-amount">¥4000</span></a>
+                <a href="割り勘明細hikaru.php"><span class="payment-amount2">¥2000</span></a>
                 </div>
             </li>
             <li class="member-item">
                 <a>はやと</a>
                 <div>
-                <a href="割り勘明細受け取りhayato.php"><span class="payment-amount">¥7000</span></a>
-                <a href="割り勘明細hayato.php"><span class="payment-amount3">¥2500</span></a>
+                <a href="割り勘明細受け取りhayato.php"><span class="payment-amount">¥8000</span></a>
+                <a href="割り勘明細hayato.php"><span class="payment-amount3">¥1000</span></a>
                 </div>
             </li>
             <li class="member-item">
                 <a>しゅうと</a>
                 <div>
-                <a href="割り勘明細受け取りsyuuto.php"><span class="payment-amount">¥7000</span></a>
-                <a href="割り勘明細syuuto.php"><span class="payment-amount4">¥2500</span></a>
+                <a href="割り勘明細受け取りsyuuto.php"><span class="payment-amount">¥0</span></a>
+                <a href="割り勘明細syuuto.php"><span class="payment-amount4">¥3000</span></a>
                 </div>
             </li>
             <li class="member-item">
                 <a>れおん</a>
                 <div>
-                <a href="割り勘明細受け取りreon.php"><span class="payment-amount">¥7000</span></a>
-                <a href="割り勘明細reon.php"><span class="payment-amount4">¥2500</span></a>
+                <a href="割り勘明細受け取りreon.php"><span class="payment-amount">¥0</span></a>
+                <a href="割り勘明細reon.php"><span class="payment-amount4">¥3000</span></a>
                 </div>
             </li>
             <li class="member-item">
                 <a>いくみ</a>
                 <div>
-                <a href="割り勘明細受け取りikumi.php"><span class="payment-amount">¥7000</span></a>
-                <a href="割り勘明細ikumi.php"><span class="payment-amount4">¥2500</span></a>
+                <a href="割り勘明細受け取りikumi.php"><span class="payment-amount">¥0</span></a>
+                <a href="割り勘明細ikumi.php"><span class="payment-amount4">¥3000</span></a>
                 </div>
             </li>
         </ul>
