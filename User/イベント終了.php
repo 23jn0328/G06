33eed2ea8e80c7e7c6a06c9e9f8d5a8d50d9aef1
鈴木輝 <23jn0328@jec.ac.jp>
2025-02-14 16:11:41 +0900

    明細ボタン消した

--- User/イベント終了.php
+++ User/イベント終了.php
@@ -172,15 +172,6 @@ body {
         </a>
     </div>
 
-    <!-- 説明テキスト -->
-    <div id="text-center">
-        <small>
-            <span class="text-blue">青字</span>は受け取り金額
-            <span class="mx-1">/</span>
-            <span class="text-red">赤字</span>は支払い金額
-        </small>
-    </div>
-
     <ul class="member-list">
         <!-- 作成者 -->
         <?php if ($creatorName): ?>
@@ -188,14 +179,6 @@ body {
                 <!-- チェックボックスを追加 -->
                 <input type="checkbox" class="transaction-checkbox" data-amount="<?php echo htmlspecialchars($totalAmount); ?>">
                 <a><?= htmlspecialchars($creatorName, ENT_QUOTES, 'UTF-8') ?></a>
-                <div>
-                    <a href="割り勘明細受け取り.php">
-                        <span class="payment-amount">受け取り</span>
-                    </a>
-                    <a href="割り勘明細.php">
-                        <span class="payment-amount2">支払い</span>
-                    </a>
-                </div>
             </li>
         <?php endif; ?>
 
@@ -206,14 +189,8 @@ body {
                     <!-- チェックボックスを追加 -->
                     <input type="checkbox" class="transaction-checkbox" data-amount="<?php echo htmlspecialchars($totalAmount); ?>">
                     <a><?= htmlspecialchars($member['EventMemberName'] ?? '不明なメンバー', ENT_QUOTES, 'UTF-8') ?></a>
-                    <div>
-                    <a href="割り勘明細受け取り.php">
-                        <span class="payment-amount">受け取り</span>
-                    </a>
-                    <a href="割り勘明細.php">
-                        <span class="payment-amount2">支払い</span>
-                    </a>
-                    </div>
+                    
+                    
                 </li>
             <?php endforeach; ?>
         <?php else: ?>
